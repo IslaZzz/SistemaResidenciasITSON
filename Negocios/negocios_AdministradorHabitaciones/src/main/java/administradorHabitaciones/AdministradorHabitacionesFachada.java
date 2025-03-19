52c44000c9d00174cce93aab84eb6512a373e7dd
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-03-18 19:22:57 -0700

    Correccion
    Quitando implementaciones

--- Negocios/negocios_AdministradorHabitaciones/src/main/java/administradorHabitaciones/AdministradorHabitacionesFachada.java
+++ Negocios/negocios_AdministradorHabitaciones/src/main/java/administradorHabitaciones/AdministradorHabitacionesFachada.java
@@ -30,6 +30,7 @@ public class AdministradorHabitacionesFachada {
 
     /**
      * Asigna un residente a una habitación si hay espacio disponible.
+<<<<<<< Updated upstream
      * La fachada simplemente delega la operación a la clase BO.
      * @param idResidente el identificador del residente.
      * @param idHabitacion el identificador de la habitación.
@@ -38,12 +39,32 @@ public class AdministradorHabitacionesFachada {
         boolean asignado = habitacionBO.asignarResidente(idResidente, idHabitacion);
         if (asignado) {
             System.out.println("Residente " + idResidente + " asignado a la habitación " + idHabitacion);
+=======
+     * @param idResidente el identificador del residente.
+     * @param idHabitacion el identificador de la habitación.
+     */
+    @Override
+    public void asignarHabitacion(int idResidente, int idHabitacion) {
+        HabitacionDTO habitacion = habitacionBO.obtenerHabitacion(idHabitacion);
+        if (habitacion != null) {
+            // Crear el objeto ResidenteDTO con el idResidente
+            ResidenteDTO residente = new ResidenteDTO(idResidente);
+            
+            // Asignar la habitación
+            boolean asignado = habitacion.agregarResidente(residente);
+            if (asignado) {
+                System.out.println("Residente " + idResidente + " asignado a habitación " + idHabitacion);
+            } else {
+                System.out.println("La habitación " + idHabitacion + " ya está ocupada por dos estudiantes.");
+            }
+>>>>>>> Stashed changes
         } else {
             System.out.println("No se pudo asignar al residente " + idResidente + " a la habitación " + idHabitacion);
         }
     }
 
     /**
+<<<<<<< Updated upstream
      * Libera a un residente de una habitación.
      * @param idResidente el identificador del residente.
      * @param idHabitacion el identificador de la habitación.
@@ -52,6 +73,27 @@ public class AdministradorHabitacionesFachada {
         boolean liberado = habitacionBO.liberarResidente(idResidente, idHabitacion);
         if (liberado) {
             System.out.println("Residente " + idResidente + " ha salido de la habitación " + idHabitacion);
+=======
+     * Libera a un estudiante de su habitación.
+     * @param idResidente el identificador del estudiante.
+     * @param idHabitacion el identificador de la habitación.
+     */
+
+    @Override
+    public void liberarHabitacion(int idResidente, int idHabitacion) {
+        HabitacionDTO habitacion = habitacionBO.obtenerHabitacion(idHabitacion);
+        if (habitacion != null) {
+            // Crear el objeto ResidenteDTO con el idEstudiante
+            ResidenteDTO residente = new ResidenteDTO(idResidente);
+            
+            // Liberar la habitación
+            boolean liberado = habitacion.removerResidente(residente);
+            if (liberado) {
+                System.out.println("Residente " + idResidente + " ha salido de la habitación " + idHabitacion);
+            } else {
+                System.out.println("El Residente " + idResidente + " no está en la habitación " + idHabitacion);
+            }
+>>>>>>> Stashed changes
         } else {
             System.out.println("El residente " + idResidente + " no se encuentra en la habitación " + idHabitacion);
         }
