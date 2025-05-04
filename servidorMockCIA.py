ade9e4a052a477e0f79a4eb0e737bd2f07530326
JorgeCuevasGastelum <jorge.cuevas252274@potros.itson.edu.mx>
2025-05-03 17:12:09 -0700

    Cambios a servidorMockCIA.py
    - El servidor ahora recibe un JSON con la matricula del estudiante para buscarlo y regresarlo

--- servidorMockCIA.py
+++ servidorMockCIA.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ alumnos_mock = {
         "carrera": "Ingenieria en Software",
         "correo": "jorge@potros.itson.edu.mx",
         "telefono": "6441222916",
-        "direccion": "Av. Antonio Caso 1354"
+        "direccion": "Av. Yaqui 1024"
     },
     "00000252687": {
         "matricula": "00000252687",
@@ -23,16 +23,51 @@ alumnos_mock = {
         "correo": "pedro@potros.itson.edu.mx",
         "telefono": "6442531964",
         "direccion": "Av. Yaqui 1124"
+    },
+    "00000252825": {
+        "matricula": "00000252825",
+        "nombre": "Ari Montoya",
+        "sexo": "H",
+        "semestre": "4",
+        "carrera": "Ingenieria en Software",
+        "correo": "ari@potros.itson.edu.mx",
+        "telefono": "6441653126",
+        "direccion": "Av. Yaqui 1324"
+    },
+    "00000253017": {
+        "matricula": "00000253017",
+        "nombre": "Abril Reyes",
+        "sexo": "M",
+        "semestre": "4",
+        "carrera": "Ingenieria en Software",
+        "correo": "abril@potros.itson.edu.mx",
+        "telefono": "6311111405",
+        "direccion": "Av. Yaqui 1824"
+    },
+    "00000241556": {
+        "matricula": "00000241556",
+        "nombre": "Victoria Valenzuela",
+        "sexo": "M",
+        "semestre": "4",
+        "carrera": "Ingenieria en Software",
+        "correo": "victoria@potros.itson.edu.mx",
+        "telefono": "6444471622",
+        "direccion": "Av. Yaqui 2024"
     }
 }
 
-@app.route('/alumno/<matricula>', methods=['GET'])
-def obtener_alumno(matricula):
+@app.route('/buscar-alumno', methods=['POST'])
+def buscar_alumno():
+    datos = request.get_json()
+    matricula = datos.get("matricula")
+
     alumno = alumnos_mock.get(matricula)
     if alumno:
-        return jsonify(alumno)
+        # regresa el alumno y el codigo 200 para confirmar que se encontro
+        return jsonify(alumno), 200
     else:
+        # regresa el codigo 404 para confirmar que no se encontro
         return jsonify({"error": "Alumno no encontrado"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(port=5000)
