d28fd288908ba0af7a37da3f71dd5a03ac385966
voiddstles <victoria.vzls24@gmail.com>
2025-05-12 21:09:29 -0700

    documentacion control reporte mantenimiento

--- Presentacion/presentacion_residencias/src/main/java/control/ControlReporteMantenimiento.java
+++ Presentacion/presentacion_residencias/src/main/java/control/ControlReporteMantenimiento.java
@@ -6,24 +6,34 @@ package control;
 
 import administradorHabitaciones.AdministradorHabitacionesFachada;
 import administradorHabitaciones.IAdministradorHabitaciones;
-import dto.HabitacionDTO;
-import dto.ResidenteDTO;
-import excepciones.NegocioException;
 import java.util.List;
 import presentacion.FrmReporteMantenimiento;
 
 /**
- *
+ * Controlador para gestionar el flujo de reportes de mantenimiento.
+ * Administra la interaccion con la pantalla de reportes y consulta informacion
+ * de pisos disponibles a traves de la fachada de administrador de habitaciones.
  * @author rauln
  */
 public class ControlReporteMantenimiento {
 
+    /**
+     * Ventana para gestionar reportes de mantenimiento.
+     */
     private FrmReporteMantenimiento frameReporteMantenimiento;
 
+    /**
+     * Constructor que inicializa la pantalla de reportes de mantenimiento.
+     * Crea una instancia de FrmReporteMantenimiento asociada a este controlador.
+     */
     public ControlReporteMantenimiento() {
         frameReporteMantenimiento = new FrmReporteMantenimiento(this);
     }
 
+    /**
+     * Inicia el flujo mostrando la pantalla de reportes de mantenimiento.
+     * Configura la ventana como no redimensionable y centrada en la pantalla.
+     */
     public void iniciarFlujo() {
         frameReporteMantenimiento.setVisible(true);
         frameReporteMantenimiento.setResizable(false);
@@ -31,6 +41,10 @@ public class ControlReporteMantenimiento {
 
     }
 
+    /**
+     * Obtiene una lista de todos los pisos disponibles.
+     * @return Lista de numeros de pisos disponibles
+     */
     public List<Integer> obtenerPisos(){
         IAdministradorHabitaciones adminHabitaciones = new AdministradorHabitacionesFachada();
         return adminHabitaciones.obtenerTodosLosPisos();
