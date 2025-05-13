02ce189209f0370859d1a485443480c326a5e2c5
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-05-12 19:56:55 -0700

    Documentacion negocios_AdministradorPersonal

--- Negocios/negocios_AdministradorPersonal/src/main/java/itson/negocios_administradorpersonal/AdministradorPersonalFachada.java
+++ Negocios/negocios_AdministradorPersonal/src/main/java/itson/negocios_administradorpersonal/AdministradorPersonalFachada.java
@@ -6,10 +6,21 @@ import exceptions.NoEncontradoException;
 import java.util.List;
 import objetosnegocio.PersonalBO;
 
+/**
+ * Fachada que implementa la interfaz {@link IAdministradorPersonal} para
+ * gestionar las operaciones relacionadas con el personal en el sistema. Esta
+ * clase actúa como un intermediario entre la capa de negocio (representada por
+ * {@link PersonalBO}) y la capa de acceso a datos.
+ *
+ * La clase proporciona métodos para obtener, registrar y actualizar la
+ * información de los empleados en el sistema, delegando las acciones
+ * correspondientes a la lógica de negocio encapsulada en {@link PersonalBO}.
+ */
 public class AdministradorPersonalFachada implements IAdministradorPersonal {
 
     /**
-     * Instancia de la clase PersonalBO que maneja la lógica de negocio relacionada con el personal.
+     * Instancia de la clase PersonalBO que maneja la lógica de negocio
+     * relacionada con el personal.
      */
     private PersonalBO personalBO = PersonalBO.getInstance();
 
@@ -22,25 +33,29 @@ public class AdministradorPersonalFachada implements IAdministradorPersonal {
     /**
      * Método que obtiene un miembro del personal específico del sistema.
      *
-     * @param personalDTO El objeto DTO que representa al miembro del personal a buscar.
+     * @param personalDTO El objeto DTO que representa al miembro del personal a
+     * buscar.
      * @return El objeto DTO del miembro del personal encontrado.
+     * @throws excepciones.NegocioException
      */
     @Override
     public PersonalDTO obtenerPersonal(PersonalDTO personalDTO) throws NegocioException {
         try {
             return personalBO.obtenerPersonal(personalDTO);
-        } catch (NoEncontradoException e){
-            throw new NegocioException("Error al encontrar personal: "+e.getMessage());
+        } catch (NoEncontradoException e) {
+            throw new NegocioException("Error al encontrar personal: " + e.getMessage());
         }
     }
 
     /**
-     * Método que obtiene una lista de todos los miembros del personal registrados en el sistema.
+     * Método que obtiene una lista de todos los miembros del personal
+     * registrados en el sistema.
      *
+     * @param puesto
      * @return Una lista de objetos DTO de miembros del personal.
      */
     @Override
     public List<PersonalDTO> obtenerPersonalPorPuesto(String puesto) {
         return personalBO.obtenerPersonalPorPuesto(puesto);
     }
-} 
+}
--- Negocios/negocios_AdministradorPersonal/src/main/java/itson/negocios_administradorpersonal/IAdministradorPersonal.java
+++ Negocios/negocios_AdministradorPersonal/src/main/java/itson/negocios_administradorpersonal/IAdministradorPersonal.java
@@ -4,19 +4,31 @@ import dto.PersonalDTO;
 import excepciones.NegocioException;
 import java.util.List;
 
+/**
+ * Interfaz que define los métodos para gestionar las operaciones relacionadas
+ * con el personal en el sistema. Esta interfaz establece los contratos para
+ * obtener, registrar y actualizar la información de los empleados.
+ *
+ * La implementación de esta interfaz debe delegar las acciones correspondientes
+ * a la capa de negocio que maneja la lógica de personal.
+ */
 public interface IAdministradorPersonal {
-    
+
     /**
      * Obtiene un miembro del personal específico del sistema.
      *
-     * @param personalDTO El objeto DTO que representa al miembro del personal a buscar.
+     * @param personalDTO El objeto DTO que representa al miembro del personal a
+     * buscar.
      * @return El objeto DTO del miembro del personal encontrado.
+     * @throws excepciones.NegocioException
      */
     PersonalDTO obtenerPersonal(PersonalDTO personalDTO) throws NegocioException;
 
     /**
-     * Obtiene una lista de todos los miembros del personal registrados en el sistema.
+     * Obtiene una lista de todos los miembros del personal registrados en el
+     * sistema.
      *
+     * @param puesto
      * @return Una lista de objetos DTO de miembros del personal.
      */
     List<PersonalDTO> obtenerPersonalPorPuesto(String puesto);
