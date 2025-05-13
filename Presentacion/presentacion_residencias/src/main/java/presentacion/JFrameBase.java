9bb21dd547f118938c14540b8e82be9df76d0b59
voiddstles <victoria.vzls24@gmail.com>
2025-05-12 21:58:48 -0700

    documentacion jframe base

--- Presentacion/presentacion_residencias/src/main/java/presentacion/JFrameBase.java
+++ Presentacion/presentacion_residencias/src/main/java/presentacion/JFrameBase.java
@@ -7,11 +7,16 @@ package presentacion;
 import javax.swing.JFrame;
 
 /**
- *
+ * Clase base para ventanas de la aplicacion.
+ * Establece un tamano predeterminado, centra la ventana, deshabilita el redimensionamiento y configura el cierre del sistema al cerrar la ventana.
  * @author abrilislas
  */
 public class JFrameBase extends JFrame{
     
+        /**
+        * Crea una nueva ventana con configuraciones predeterminadas.
+        * Establece un tamano de 911x634 pixeles, centra la ventana en la pantalla, deshabilita el redimensionamiento y configura el cierre del sistema al cerrar la ventana.
+        */
         public JFrameBase() {
         // Tamaño predeterminado
         this.setSize(911, 634);
