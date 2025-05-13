2b8ed1c5dec7a38460ad8c0be1014ef2ca672313
RaulNav <ari.montoya252825@potros.itson.edu.mx>
2025-05-12 19:58:51 -0700

    Documentacion administrador_zonas

--- Negocios/negocios_AdministradorZonas/src/main/java/itson/negocios_administradorzonas/AdministradorZonasFachada.java
+++ Negocios/negocios_AdministradorZonas/src/main/java/itson/negocios_administradorzonas/AdministradorZonasFachada.java
@@ -6,17 +6,30 @@ import exceptions.NoEncontradoException;
 import java.util.List;
 import objetosnegocio.ZonasBO;
 
+/**
+ * Fachada que implementa la interfaz {@link IAdministradorZonas} para gestionar
+ * las operaciones relacionadas con las zonas en el sistema. Esta clase actúa
+ * como intermediario entre la capa de negocio y la capa de acceso a datos,
+ * delegando las acciones correspondientes a la implementación de la lógica de
+ * negocio en la clase {@link ZonasBO}.
+ *
+ * La clase proporciona métodos para obtener información sobre las zonas,
+ * incluyendo obtener una zona específica o todas las zonas registradas en el
+ * sistema.
+ */
 public class AdministradorZonasFachada implements IAdministradorZonas {
 
     /**
-     * Instancia de la clase ZonaBO que maneja la lógica de negocio relacionada con las zonas.
+     * Instancia de la clase ZonaBO que maneja la lógica de negocio relacionada
+     * con las zonas.
      */
     private ZonasBO zonaBO = ZonasBO.getInstance();
-    
+
     /**
      * Método que obtiene una zona específica del sistema.
      *
      * @return ZonaDTO que representa la zona encontrada.
+     * @throws excepciones.NegocioException
      */
     @Override
     public ZonaDTO obtenerZona(ZonaDTO zona) throws NegocioException {
@@ -26,10 +39,11 @@ public class AdministradorZonasFachada implements IAdministradorZonas {
             throw new NegocioException("Error al obtener la zona: " + e.getMessage());
         }
     }
-    
+
     /**
-     * Método que obtiene una lista de todas las zonas registradas en el sistema.
-     * 
+     * Método que obtiene una lista de todas las zonas registradas en el
+     * sistema.
+     *
      * @return Una lista de zonas.
      */
     @Override
--- Negocios/negocios_AdministradorZonas/src/main/java/itson/negocios_administradorzonas/IAdministradorZonas.java
+++ Negocios/negocios_AdministradorZonas/src/main/java/itson/negocios_administradorzonas/IAdministradorZonas.java
@@ -4,12 +4,19 @@ import dto.ZonaDTO;
 import excepciones.NegocioException;
 import java.util.List;
 
+/**
+ * Interfaz que define las operaciones relacionadas con la gestión de zonas en
+ * el sistema. Proporciona métodos para obtener zonas específicas o una lista
+ * completa de todas las zonas registradas.
+ */
 public interface IAdministradorZonas {
 
     /**
      * Obtiene una zona específica del sistema.
+     *
      * @param zona ZonaDTO que representa la zona a buscar.
      * @return ZonaDTO que representa la zona encontrada.
+     * @throws excepciones.NegocioException
      */
     ZonaDTO obtenerZona(ZonaDTO zona) throws NegocioException;
 
