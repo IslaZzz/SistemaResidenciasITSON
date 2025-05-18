53f8bd68cf4068da389b4dd4cc874cc45611384b
voiddstles <victoria.vzls24@gmail.com>
2025-05-17 18:25:52 -0700

    revert cambios

--- Negocios/negocios_AdministradorResidentes/src/main/java/administradorResidentes/IAdministradorResidentes.java
+++ Negocios/negocios_AdministradorResidentes/src/main/java/administradorResidentes/IAdministradorResidentes.java
@@ -56,14 +56,6 @@ public interface IAdministradorResidentes {
      */
     void registrarResidente(ResidenteDTO residente);
     
-    /**
-     * Actualiza la información de un residente existente en el sistema.
-     *
-     * @param residente El DTO con los datos actualizados del residente.
-     * @throws NegocioException Si ocurre un error al actualizar el residente.
-     */
-    void actualizarResidente(ResidenteDTO residente) throws NegocioException;
-
     List<String> obtenerResidentesPorHabitacion(Integer piso, Integer habitacion);
 
 }
